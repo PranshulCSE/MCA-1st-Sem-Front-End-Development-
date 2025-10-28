f23be786fc5e9655847bd73c6d36f8e08420a4d7
Pranshul061204 <threjapranshul@gmail.com>
2025-10-28 15:17:14 +0530

    Started Learning JavaScript from Today

---
+++ java_Script/TypesofJS/External_JS/script.js
@@ -0,0 +1,3 @@
+function write() {
+            document.getElementById("B1").innerHTML = "Hello I'm Learning JavaScript";
+        }
--- java_Script/js_Variables.js
+++
@@ -1,6 +0,0 @@
-let a="Hello";
-var b=1322;
-const c=true;
-console.log(a);
-console.log(b);
-console.log(c); 
