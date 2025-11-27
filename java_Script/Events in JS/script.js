6e88aed5446c07deeedfde0d633ec8185f7bb065
Pranshul061204 <threjapranshul@gmail.com>
2025-11-27 22:33:29 +0530

    Practicing JavaScript Event Listener

--- java_Script/Events in JS/script.js
+++ java_Script/Events in JS/script.js
@@ -1,4 +1,27 @@
-function click(){
+// Not Recommended
+// function click(){
+//           const par=document.getElementById("para");
+//           PageTransitionEvent.textContent="Hello!! I am Learning JavaScript Events";
+//         }
+
+// 2nd Method (Also not Recommended)
+          // const par=document.getElementById("para");
+          // par.onclick=function handle(){
+          // par.textContent="Hello!! I am Learning JavaScript Events";
+          // }
+          // because in this we cannot add multiple functions,if we write then it will be overwritten;
+
+// 3rd Method
           const par=document.getElementById("para");
-          par.textContent="Hello!! I am Learning JavaScript Events";
-        }
+          par.addEventListener('click',()=>{
+           par.textContent="Hello!! I am Learning JavaScript Events";
+ 
+          })
+                 
+          par.addEventListener('click',()=>{
+            par.style.backgroundColor="pink";
+          })
+
+            par.addEventListener('click',()=>{
+            par.style.color="blue";
+          })
---
+++ java_Script/Events in JS/script3.js
@@ -0,0 +1,14 @@
+const grandparent=document.getElementById("grandparent");
+grandparent.addEventListener('click',()=>{
+    console.log("Dada ji is Clicked!!");
+})
+
+const parent=document.getElementById("parent");
+grandparent.addEventListener('click',()=>{
+    console.log("Papa ji is Clicked!!");
+})
+
+const child=document.getElementById("child");
+grandparent.addEventListener('click',()=>{
+    console.log("Beta is Clicked!!");
+})
