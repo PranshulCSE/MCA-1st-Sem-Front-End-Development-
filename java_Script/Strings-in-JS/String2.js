a19c510b81c7d8c4e4568ffd9c44feb875b35430
Pranshul061204 <threjapranshul@gmail.com>
2025-11-27 14:16:09 +0530

    Practicing JavaScript Strings

---
+++ java_Script/Strings-in-JS/String2.js
@@ -0,0 +1,6 @@
+// var yourname= prompt("What is your Name?");
+var yourname="aDhIsH";
+var name=yourname.slice(0,1);
+var rest=yourname.slice(1,yourname.length);
+// alert("Hello Mr./Ms. " + name.toUpperCase() +rest.toLowerCase());
+console.log("Hello Mr./Ms. " + name.toUpperCase() +rest.toLowerCase());
