9cd51645c9bf6c1a873e1773e3d15c5cb6d78666
Pranshul061204 <threjapranshul@gmail.com>
2025-11-28 22:26:37 +0530

    Minor Updation on How can we Remove Event Listener

--- java_Script/Events in JS/script5.js
+++ java_Script/Events in JS/script5.js
@@ -1,4 +1,12 @@
         const c1=document.getElementsByClassName("first");
-         c1.addEventListener('click',(e)=>{
-            e.target.textContent="I am Clicked";
-        })
+        //  c1.addEventListener('click',(e)=>{
+        //     e.target.textContent="I am Clicked";
+        // })
+        //   To Remove addEventListener Follow the Below Code
+
+        // Transferring the code into fumction
+        function e(){
+          e.target.textContent="I am Clicked";
+        }
+         c1.addEventListener('click',e)
+         c1.removeaddEventListener('click',e)
