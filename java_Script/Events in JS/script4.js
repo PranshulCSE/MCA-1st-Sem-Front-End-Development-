90d83fbb0db98a522f1404a95d754e5a4ae9926a
Pranshul061204 <threjapranshul@gmail.com>
2025-11-28 22:22:40 +0530

    Practicing JavaScript

--- java_Script/Events in JS/script4.js
+++ java_Script/Events in JS/script4.js
@@ -2,5 +2,6 @@ const grandpa=document.getElementById("gt");
 grandpa.addEventListener('click',(obj)=>{
     console.log(obj);
     console.log("Dada ji is Clicked!!");
+    // obj.stopPropagation(); To Stop Bubbling
 })
 // Here the AddEvent Listener Returns an Object which is used for different properties & methods related to this Event.
---
+++ java_Script/Events in JS/script5.js
@@ -0,0 +1,4 @@
+        const c1=document.getElementsByClassName("first");
+         c1.addEventListener('click',(e)=>{
+            e.target.textContent="I am Clicked";
+        })
