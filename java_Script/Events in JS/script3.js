85e1e9aee46fedc6ce383304133d155cec622c19
Pranshul061204 <threjapranshul@gmail.com>
2025-11-28 20:00:59 +0530

    Practicing JavaScript AddEvent Listener

--- java_Script/Events in JS/script3.js
+++ java_Script/Events in JS/script3.js
@@ -1,14 +1,31 @@
+// Understanding the Event Bubbling
+// const grandparent=document.getElementById("grandparent");
+// grandparent.addEventListener('click',()=>{
+//     console.log("Dada ji is Clicked!!");
+// })
+
+// const parent=document.getElementById("parent");
+// grandparent.addEventListener('click',()=>{
+//     console.log("Papa ji is Clicked!!");
+// })
+
+// const child=document.getElementById("child");
+// grandparent.addEventListener('click',()=>{
+//     console.log("Beta is Clicked!!");
+// })
+
+// To Change the Ordering of EventBubbling
 const grandparent=document.getElementById("grandparent");
 grandparent.addEventListener('click',()=>{
     console.log("Dada ji is Clicked!!");
-})
+},true)
 
 const parent=document.getElementById("parent");
 grandparent.addEventListener('click',()=>{
     console.log("Papa ji is Clicked!!");
-})
+},true)
 
 const child=document.getElementById("child");
 grandparent.addEventListener('click',()=>{
     console.log("Beta is Clicked!!");
-})
+},true)
---
+++ java_Script/Events in JS/script4.js
@@ -0,0 +1,6 @@
+const grandpa=document.getElementById("gt");
+grandpa.addEventListener('click',(obj)=>{
+    console.log(obj);
+    console.log("Dada ji is Clicked!!");
+})
+// Here the AddEvent Listener Returns an Object which is used for different properties & methods related to this Event.
